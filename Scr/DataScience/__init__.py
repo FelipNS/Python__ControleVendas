641ec57d46d9dd947938ed14e4b22e1ce1073abb
Felipe <felipens0806@gmail.com>
2022-01-02 20:01:06 -0300

    update Data Science

--- Scr/DataScience/__init__.py
+++ Scr/DataScience/__init__.py
@@ -3,13 +3,13 @@ from pymongo import MongoClient
 import mysql.connector
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 COLUMNS_NAME = ['Nº da Comanda',  'Vendedor', 'Combo', 'F. de Pagamento', 'Preço', 'Tamanho', 'Bairro', 'Cereais', 'Chocolates', 'Coberturas', 'Derivados do Leite', 'Diversos', 'Frutas', 'Obs', 'Data']
 
 SELLER = ('seller', 'Vendedor')
-COMBO = ('combo', 'Combo')
-PAYMENT_TYPE = ('payment_type', 'F. de Pagamento')
-PRICE = ('price', 'Preço')
+COMBO = ('combos', 'Combo')
+PAYMENT_OPTION = ('payment_option', 'F. de Pagamento')
 SIZE = ('size', 'Tamanho')
 NEIGHBORHOOD = ('neighborhood', 'Bairro')
 
@@ -24,29 +24,29 @@ class ManipulationDB:
         #Connect MySQL
         self.conn = mysql.connector.connect(host='localhost', user='root', passwd='', database='acaiteria')
     
-    def create_dataframe(self, with_seller_name=False) -> pd.DataFrame:
+    def create_dataframe(self, name_or_id_seller='id') -> pd.DataFrame:
         """Create data frame with data of sells
 
         Args:
-            with_seller_name (bool, optional): [description]. Defaults to False.
+            name_or_id_seller (str, optional): Supports 'name' or 'id' If name, the seller column in dataframe will receive seller's name, if not, it will receive the seller id. Defaults to 'id'.
 
         Returns:
-            pd.DataFrame: [description]
+            pd.DataFrame
         """
         i = dict()
         collection = self.db['comandas']
         cursor = collection.find({})
 
-        df_main = pd.DataFrame(self.__pretty_json(cursor[0], with_seller_name), columns=COLUMNS_NAME)
+        df_main = pd.DataFrame(self.__pretty_json(cursor[0], name_or_id_seller), columns=COLUMNS_NAME)
 
         for i in cursor[1::]:
-            df_temp = pd.DataFrame(self.__pretty_json(i, with_seller_name), columns=COLUMNS_NAME)
+            df_temp = pd.DataFrame(self.__pretty_json(i, name_or_id_seller), columns=COLUMNS_NAME)
             df_main = pd.concat([df_main, df_temp])
         
         cursor.close()
         return df_main
 
-    def __pretty_json(self, sheet: str, with_name=False) -> dict:
+    def __pretty_json(self, sheet: str, with_name='id') -> dict:
         """Create a pretty json format
 
         Args:
@@ -59,7 +59,7 @@ class ManipulationDB:
         old_json = sheet
         pretty_sheet = dict()
         pretty_sheet['number_sheet'] = old_json['n_comanda']
-        pretty_sheet['seller'] = self.__name_seller(old_json['seller']) if with_name else old_json['seller']
+        pretty_sheet['seller'] = self._name_seller(old_json['seller']) if with_name  == 'name' else old_json['seller']
         pretty_sheet['combo'] = old_json['combo']
         pretty_sheet['payment_type'] = old_json['f_pagamento']
         pretty_sheet['price'] = old_json['preco']
@@ -101,11 +101,11 @@ class ManipulationDB:
 
         return (sales_quantity, revenue, mean_sales)
     
-    def quantity_type(self, names: tuple)-> pd.DataFrame:
-        """Create data frame groupy by any colum type
+    def count_type(self, names: tuple)-> pd.DataFrame:
+        """Create data frame group by any colum type
 
         Args:
-            names (tuple): Value used to group the column. Suppot values: SELLER, COMBO, PAYMENT_TYPE, PRICE, SIZE, NEIGHBORHOOD
+            names (tuple): Value used to group the column. Suppot values: COMBO, COMBO, PAYMENT_TYPE, PRICE, SIZE, NEIGHBORHOOD
 
         Returns:
             pd.DataFrame: Data frame group by column 
@@ -118,8 +118,8 @@ class ManipulationDB:
             types_names = list(i for i in cursor[0].keys() if i != '_id')
         else:
             cursor = self.conn.cursor()
-            cursor.execute('SELECT primeiro_nome, sobrenome FROM empregados')
-            types_names = list(map(lambda x: f'{x[0]} {x[1]}', cursor))
+            cursor.execute('SELECT id FROM empregados')
+            types_names = list(map(lambda x: x[0], cursor))
         if names == SIZE:
             types_names = list(map(self.__str_numeric, types_names))
         for i in types_names:
@@ -128,10 +128,11 @@ class ManipulationDB:
                 if j == i:
                     qtd += 1
             quantitys[i] = qtd 
-        quantity_any_thing = pd.DataFrame.from_dict(quantitys, orient='index', columns=['Qtd.'])
-        
+        quantity_any_thing = pd.DataFrame.from_dict(quantitys, orient='index', columns=['Qtd'])
+        quantity_any_thing['index'] = quantity_any_thing.index
+
         cursor.close()
-        return quantity_any_thing
+        return quantity_any_thing.sort_values(by='Qtd')
     
     def __str_numeric(self, str: str) -> int:
         """Return only number of a str
@@ -150,7 +151,7 @@ class ManipulationDB:
             only_number = '1000'
         return int(only_number)
     
-    def __name_seller(self, id: int) -> str:
+    def _name_seller(self, id: int) -> str:
         """Return name of seller according to ID
 
         Args:
@@ -166,8 +167,28 @@ class ManipulationDB:
         cursor.close()
         return employee_name[0]
 
-dataScience = ManipulationDB()
+class Graphics:
+    def __init__(self, type_data) -> None:
+        self.type_data = type_data  
+        self.data = ManipulationDB().count_type(self.type_data)
 
-#dataScience.export_excel(dataScience.create_dataframe(), 'C:/Users/USER/OneDrive/Documentos/Programação/Python/Projetos/Comandas-Açai/Scr/SupportFiles/comandas.xlsx')
+    def bar_graphic(self):
+        if self.type_data == SELLER:
+            names = list(map(lambda x: x.split(' ')[0], map(ManipulationDB()._name_seller, self.data['index'])))
+        else:
+            names = self.data['index']
+        plt.bar(names, self.data['Qtd'], color='blue')
+        plt.xticks(rotation='vertical')
+        plt.xlabel('Vendedores')
+        plt.ylabel('Qtd')
+        plt.title('Quantidade vendida por funcionario')
+        plt.show()
+    
+    def pie_graphic(self):
+        pass
 
-print(dataScience.quantity_type(SELLER))
+#GP = Graphics(PAYMENT_OPTION)
+#MD = ManipulationDB()
+#MD.export_excel(MD.create_dataframe('name'), r'C:\Users\USER\OneDrive\Documentos\Programação\Python\Projetos\Comandas-Açai\Dashboard\comandas.xlsx')
+
+GRAPH = Graphics(PAYMENT_OPTION).bar_graphic()
